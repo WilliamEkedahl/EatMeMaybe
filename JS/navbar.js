2625805657706185159b78df83181181ff8e94f5
martin ullersmo <martin_ullersmo@hotmail.com>
2025-03-06 14:13:03 +0100

    Fixed some changes from last commit

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,3 +1,17 @@
+function dropdownFunction() {
+  document.getElementById("menuDropdown").classList.toggle("show");
+}
+
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var menuDropdown = document.getElementById("menuDropdown");
+    if (menuDropdown.classList.contains('show')) {
+      menuDropdown.classList.remove('show');
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.querySelector(".menuButton");
   const sidebar = document.querySelector(".sidebar");
