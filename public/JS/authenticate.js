657f0cd9bd00d7166077c3cf4ad7be112dc848a7
WilliamEkedahl <altinus.ekedahl@gmail.com>
2025-05-23 15:47:55 +0200

    idk changes

--- public/JS/authenticate.js
+++ public/JS/authenticate.js
@@ -21,7 +21,7 @@ if (signUpBtn) {
 //signInButton
 const signInBtn = document.getElementById('signInButton');
 if (signInBtn) {
-    signInBtn.addEventListener('click', function (event) {
+    signInBtn.addEventListener('submit', function (event) {
         event.preventDefault();
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
