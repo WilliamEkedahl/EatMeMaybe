22d1832d6bff06d65f4f04858b6179236ce70ebd
Atle Leerstang <250724@gmail.com>
2025-05-28 15:25:07 +0200

    changed url links in navbar menu

--- public/JS/navbar.js
+++ public/JS/navbar.js
@@ -1,26 +1,24 @@
+
 function dropdownFunction() {
   document.getElementById("menuDropdown").classList.toggle("show");
 }
-
-
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var menuDropdown = document.getElementById("menuDropdown");
-    if (menuDropdown.classList.contains('show')) {
-      menuDropdown.classList.remove('show');
-    }
+ 
+document.addEventListener('click', function(e) {
+  const menuDropdown = document.getElementById("menuDropdown");
+  if (!e.target.closest('.dropbtn') && !menuDropdown.contains(e.target)) {
+    menuDropdown.classList.remove('show');
   }
-}
-
+});
+ 
 document.addEventListener("DOMContentLoaded", function () {
   const menuButton = document.querySelector(".menuButton");
   const sidebar = document.querySelector(".sidebar");
-
+ 
   menuButton.addEventListener("click", function (event) {
-    event.stopPropagation(); // Hindrer at klikking på knappen lukker menyen
+    event.stopPropagation(); // Hindrer at klikk på knappen lukker menyen
     sidebar.classList.toggle("show");
   });
-
+ 
   // Klikk utenfor sidebar for å lukke den
   document.addEventListener("click", function (event) {
     if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
@@ -28,3 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+ 
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.remove("show");
+}
