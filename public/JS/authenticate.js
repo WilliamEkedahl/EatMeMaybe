6d6a323fd0eb36a43fd11ee7f2f39d210af9e2c0
WilliamEkedahl <altinus.ekedahl@gmail.com>
2025-05-29 15:35:35 +0200

    fixed authenticate.js

--- public/JS/authenticate.js
+++ public/JS/authenticate.js
@@ -5,7 +5,7 @@ import{
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-au.js";
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
 import { doc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
