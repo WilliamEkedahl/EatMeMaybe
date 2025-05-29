cd86735783b7055d34728b872c49112d893cb4de
WilliamEkedahl <altinus.ekedahl@gmail.com>
2025-05-29 15:57:52 +0200

    import from different package

--- public/JS/authenticate.js
+++ public/JS/authenticate.js
@@ -5,11 +5,9 @@ import{
     signInWithEmailAndPassword,
     signOut,
     onAuthStateChanged,
-    getDocs,
-    deleteDoc,
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
 
-import { doc, setDoc, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { doc, setDoc, collection, addDoc,  getDocs, deleteDoc, } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 export async function signUp(email, username, password){
     try {
