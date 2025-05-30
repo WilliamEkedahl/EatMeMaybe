4a189e75fef08f11a4f3c8951cd3a521a6b97d8b
Nygrenmn <nygren.mn@gmail.com>
2025-05-30 14:32:26 +0200

    changed title and colour in index

--- public/JS/userInventory.js
+++ public/JS/userInventory.js
@@ -59,7 +59,7 @@ auth.onAuthStateChanged(async user => {
                 const username = userDoc.data().username;
                 const inventoryTitle = document.getElementById("inventory-title");
                 if (inventoryTitle) {
-                    inventoryTitle.textContent = `${username}'s inventory`;
+                    inventoryTitle.textContent = `Welcome back, ${username}! Here's your inventory`;
                 }
             }
         } catch (error) {
