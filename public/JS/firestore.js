dabde3904161296ed9cd66d31e5f960e1c20be09
WilliamEkedahl <altinus.ekedahl@gmail.com>
2025-06-01 22:24:36 +0200

    added comments to firestore.js

--- public/JS/firestore.js
+++ public/JS/firestore.js
@@ -2,7 +2,30 @@
 /**
  * @author Martin U
  * @author William
-*/
+ *
+ * EatMeMaybe depends on firebase-app, Firebase-auth and firebase-firestore, using firebase modular style
+ *
+ * Firebase app is a firebase package that handles and coordinates the communication between the different firebase components
+ * it initializes the connection to the firebase services by sending in our firebaseConfig information as an argument.
+ *
+ * Firebase Authentication handles the Authentication and is mainly used to authenticate that the user is logged in,
+ * handling our account manager use cases FR1-5 in authenticate.js, as well as handling authentication in userInventory.js and Search.js.
+ *
+ *Firebase Firestore
+ * Intializes our cloud-hosted NOSQL database that is used to store the userInventory, user information and the available products.
+ *
+ *
+ * FirebaseApp consists of the initialization information for a bunch of services,
+ * Properties:
+ * automaticDataCollectionEnabled -Boolean, A config flag to opt in/out of GDPR default set to TRUE:
+ * name - -String, A custom name for the application
+ * options -  The firebase config information to initialize the firebase connection with the database
+ *
+ * We only use the FirebaseApp Options property.
+ */
+
+
+
 
 import {initializeApp} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
 import {getAuth} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
@@ -19,9 +42,40 @@ const firebaseConfig = {
     measurementId: "G-L6PV0K8TZ1"
 };
 
-//initialize new version firebase V9
+
+/**InitializeApp()
+ * @param FirebaseOptions, ({object}, firebaseConfig)
+ * @param FirebaseAppSettings,
+ * it crates and initializes the connection to the database by sending in our firebaseConfig information as an argument FirebaseOptions
+ * that we call firebaseConfig, FirebaseAppSettings is not used by our project as a parameter
+ * but lets you enable DataCollection and set a custom name for the Firebase App.
+ */
+
+
+//initialize new version firebase V11
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+
+/**getFirestore()
+ *@param *{FirebaseApp} app, which is the initialized connection from initializeApp(),
+ *@returns *{firestore}  which is the firestore database instance used by the application to read/write/update/delete data,
+ * which is stored and exported in the constant db.
+ */
 const db = getFirestore(app);
 
+
+/**getAuth()
+ * @param *{FirebaseApp} app, which is the initialized database connection from initializeApp()
+ *@Returns *{Auth} Authentication instance used for authenticating the user, mainly used in authenticate.js and the following functions
+ * signUp, logOut, signIn, changePassword, DeleteUserInventory and deleteCurrentUser.
+ */
+const auth = getAuth(app);
+
+
+
+
+/**
+ * We Use the export keyword to export the auth and db objects
+ * so that other modules and files in our project can import and use them.
+ */
 export { auth, db };
