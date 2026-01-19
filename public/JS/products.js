faf5e8c2ce1ee731dd9f9fd7b7b058e3b82b62f6
WilliamEkedahl <altinus.ekedahl@gmail.com>
2026-01-19 02:05:09 +0100

    bug fix

--- public/JS/products.js
+++ public/JS/products.js
@@ -11,7 +11,7 @@
  * Also brings in a tool to clear saved inventory data.
  */
 import { auth, db } from "./firestore.js"
-import { collection, addDoc, TimeStamp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { collection, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 import { clearUserInventoryCache, loadProducts, getCachedProducts } from "./cache.js";
 
 // Waits until the webpage is fully loaded, then runs the main setup function
