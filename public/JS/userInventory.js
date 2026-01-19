d7144d58d120d3cc24d4fde1ecee4fd4188d2a5d
WilliamEkedahl <altinus.ekedahl@gmail.com>
2026-01-19 23:17:30 +0100

    now allows custom dates

--- public/JS/userInventory.js
+++ public/JS/userInventory.js
@@ -165,7 +165,7 @@ async function fetchUserInventory(userId) {
             const addedAt = data.addedAt.toDate();
             //testing merge
             // Get shelf life based on category or default to 7 days. Either or basically with the "??"
-            let shelfLifeDays = categoryShelfLives[data.category] ?? 7;
+            let shelfLifeDays = data.shelfLife ?? categoryShelfLives[data.category] ?? 7;
 
             // Calculate the expiration date
             const expirationDate = new Date(addedAt);
